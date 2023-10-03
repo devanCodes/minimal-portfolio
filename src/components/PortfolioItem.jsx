36db23820dae5832188aa9bd31309c798b075ab4
devanCodes <devanghailey@gmail.com>
2023-10-03 15:04:44 -0400

    Photo updates

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -18,7 +18,7 @@ function PortfolioItem({ title, imgUrl, stack, link }) {
         <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold">
           {title}
         </h3>
-        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white">
+        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white align-baseline">
           {/* mapping over the 'stack' prop, which is an array of technologies used in the portfolio item; for each 'item', it creates a 'span' element */}
           {stack.map((item) => (
             <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md">
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,23 @@
+import WellnessZoneCover from '../assets/WellnessZoneCover.PNG';
+import PortfolioCover from '../assets/PortfolioCover.PNG';
+import TicTacToeCover from '../assets/TicTacToeCover.PNG';
+
 export default [
     {
         title: 'Personal Training Website',
-        imgUrl: 'src/assets/WellnessZoneCover.PNG',
+        imgUrl: {WellnessZoneCover},
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://wellness-zone.netlify.app/',
     },
     {
         title: 'Developer Portfolio',
-        imgUrl: 'src/assets/PortfolioCover.PNG',
+        imgUrl: {PortfolioCover},
         stack: ['React', 'Tailwind', 'Vite'],
         link: '',
     },
     {
         title: 'Tic-Tac-Toe',
-        imgUrl: 'src/assets/TicTacToeCover.PNG',
+        imgUrl: {TicTacToeCover},
         stack: ['HTML', 'CSS', 'JavaScript', 'Typescript', 'React', 'Webpack'],
         link: 'https://devtictactoe.netlify.app/',
     },
