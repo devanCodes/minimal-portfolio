26f774f5a5a64224b303168813280ffa6036c738
devanCodes <devanghailey@gmail.com>
2023-10-03 15:15:57 -0400

    Portfolio updates

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,23 +1,19 @@
-import WellnessZoneCover from '../public/PortfolioCover.PNG';
-import PortfolioCover from '../public/PortfolioCover.PNG';
-import TicTacToeCover from '../public/TicTacToeCover.PNG';
-
 export default [
     {
         title: 'Personal Training Website',
-        imgUrl: {WellnessZoneCover},
+        imgUrl: '/images/WellnessZoneCover.PNG',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://wellness-zone.netlify.app/',
     },
     {
         title: 'Developer Portfolio',
-        imgUrl: {PortfolioCover},
+        imgUrl: '/images/PortfolioCover.PNG',
         stack: ['React', 'Tailwind', 'Vite'],
         link: '',
     },
     {
         title: 'Tic-Tac-Toe',
-        imgUrl: {TicTacToeCover},
+        imgUrl: '/images/TicTacToeCover.PNG',
         stack: ['HTML', 'CSS', 'JavaScript', 'Typescript', 'React', 'Webpack'],
         link: 'https://devtictactoe.netlify.app/',
     },
